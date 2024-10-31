f89f7d8c874de393cc82f853d798d2ce07cfbd7c
woollim <137277221+woollim23@users.noreply.github.com>
2024-10-31 19:58:36 +0900

    [Feature] Player : 플레이어 프리팹 생성

--- IslandSurvival/Assets/Scripts/Manager/CharacterManager.cs
+++ IslandSurvival/Assets/Scripts/Manager/CharacterManager.cs
@@ -1,4 +1,9 @@
 public class CharacterManager : Singletone<CharacterManager>
 {
-
+    public Player _player;
+    public Player Player
+    {
+        get { return _player; }
+        set { _player = value; }
+    }
 }
